cb23e5435251c266d228103ef4ab098c9aed7bce
sachin-nono <sachinghothwal@gmail.com>
2018-08-04 23:58:57 +0530

    warmup1 ques 18 - loneTeen

---
+++ warmup1/loneTeen.cpp
@@ -0,0 +1,44 @@
+/*
+    Ques - We'll say that a number is "teen" if it is in the range
+           13...19 inclusive. Given two int values, return true if one 
+           or the other is teen, but not both.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    int x, y;
+    bool a, loneTeen(int, int);
+    
+    cout<<"Enter two integers :\n";
+    cin>>x>>y;
+    
+    a=loneTeen(x,y);
+    
+    if(a==true)
+        cout<<endl<<"TRUE."<<endl;
+    else
+        cout<<endl<<"FALSE."<<endl;
+    
+    return 0;
+}
+
+bool loneTeen(int x, int y){
+    
+    int a;
+    
+    if(x>=13&&x<=19)
+        a=1;
+    
+    if(a==1){
+        if(y>=13&&y<=19)
+            return false;
+        else
+            return true;
+    }
+    else if(y>=13&&y<=19)
+        return true;
+}

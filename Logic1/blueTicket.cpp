039835d703fd27efad3033cdf11b1ba0a2994d69
sachin-nono <sachingothwal06@gmail.com>
2018-09-15 21:13:02 +0530

    ques 10 - blueTicket()

---
+++ Logic1/blueTicket.cpp
@@ -0,0 +1,29 @@
+/*
+	You have a blue lottery ticket, with ints a, b, and c
+	on it. This makes three pairs, which we'll call ab,
+	bc, and ac. Consider the sum of the numbers in each 
+	pair. If any pair sums to exactly 10, the result is 10.
+	Otherwise if the ab sum is exactly 10 more than either
+	bc or ac sums, the result is 5. Otherwise the result is 0.
+	*/
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a, b, c, blueTicket(int, int, int);
+
+	cin>>a>>b>>c;
+
+	cout<<"Result : "<<blueTicket(a,b,c)<<endl;
+
+	return 0;
+}
+
+int blueTicket(int a, int b, int c){
+	if((a+b==10)||(b+c==10)||(a+c==10))
+		return 10;
+	else if((a+b==b+c+10)||(a+b==a+c+10))
+		return 5;
+	else
+		return 0;
+}
